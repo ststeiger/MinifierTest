946a1192e963a220a70de48c916254a04cdf2eea
schmierfink <schmierfink@yandex.ru>
2026-05-07 12:28:15 +0200

    Added remarks

--- MinifierTestCore/AzureSecrets.cs
+++ MinifierTestCore/AzureSecrets.cs
@@ -1,8 +1,19 @@
 ﻿
 namespace MinifierTestCore;
+
+// for use with azure-keyvault-emulator: 
+// https://github.com/james-gould/azure-keyvault-emulator
 internal class AzureSecrets
 {
     public static readonly string EmulatorToken = "TOP_SECRET";
     public static readonly string KeyVaultUri = "https://localhost:44395";
 }
+// see also: 
+// https://github.com/cricketthomas/AzureKeyVaultExplorer/blob/master/docs/FIRST-TIME-SETUP.md
+// https://github.com/Azure/AzureKeyVault
 
+// azure-cli:
+// az keyvault secret set --vault-name https://localhost:4997 --name "TestSecret" --value "YourValue"
+
+// https://jamesgould.dev/posts/Azure-Key-Vault-Emulator/
+// https://github.com/Basis-Theory/azure-keyvault-emulator
